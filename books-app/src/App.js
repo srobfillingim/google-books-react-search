a6156da6d8fc61d6915e22b477e69b4ba3c93347
Samuel Fillingim <srobfillingim@gmail.com>
2019-07-29 23:13:15 -0500

    more books-app

--- books-app/src/App.js
+++ books-app/src/App.js
@@ -1,50 +1,23 @@
-import React from 'react';
-import axios from 'axios';
-import {Table, Button} from 'reactstrap';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Saved from "./pages/Saved";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
 
-
-// import logo from './logo.svg';
-// import './App.css';
-class App extends Component {
-  state = {
-    books: []
-  }
-  componentWillMount() {
-    axios.get('http://localhost3000/books').then(response) => {
-      this.setState ({
-        books: response.data
-      })
-    });
-  }
-
-
-render App() {
+function App() {
   return (
-    <div className="App container">
-      <Table>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Title</th>
-            <th>Rating</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          <tr>
-           <td>1</td>
-           <td>Book Title</td> 
-           <td>2.5</td>
-           <td>
-             <Button color= "success" size="sm" className="mr-2">edit</Button>
-             <Button color= "danger" size="sm">delete</Button>
-           </td>
-          </tr>
-        </tbody>
-      </Table>
-    </div>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/saved" component={Saved} />
+          <Route component={NoMatch} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
-}
+
 export default App;
---
+++ books-app/src/components/Book/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { ListItem } from "../List";
+import { Row, Col } from "../Grid";
+import "./style.css";
+
+
