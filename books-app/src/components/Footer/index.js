20d2ad3adc626d696be3772fb74fe8b31194f805
Samuel Fillingim <srobfillingim@gmail.com>
2019-07-30 00:43:26 -0500

    something not working but changed client to build app

---
+++ books-app/src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <hr />
+      <p className="pull-right">
+        <i className="fab fa-github" /> Srobfillingim 2019
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
